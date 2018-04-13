412262a22df87e820b3d400ce16e1038294e78c2
TivaR8 <tiva.rait@ocsbstudent.ca>
2018-04-13 13:00:26 -0400

    Initial commit

---
+++ ControlObjectsTivaR/ControlObjectsForm.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace ControlObjectsTivaR
+{
+    partial class frmControlObjects
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblText = new System.Windows.Forms.Label();
+            this.btnChangeColour = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblText
+            // 
+            this.lblText.AutoSize = true;
+            this.lblText.Location = new System.Drawing.Point(128, 114);
+            this.lblText.Name = "lblText";
+            this.lblText.Size = new System.Drawing.Size(77, 13);
+            this.lblText.TabIndex = 0;
+            this.lblText.Text = "Change Colour";
+            // 
+            // btnChangeColour
+            // 
+            this.btnChangeColour.Location = new System.Drawing.Point(119, 65);
+            this.btnChangeColour.Name = "btnChangeColour";
+            this.btnChangeColour.Size = new System.Drawing.Size(95, 23);
+            this.btnChangeColour.TabIndex = 1;
+            this.btnChangeColour.Text = "Change Colour";
+            this.btnChangeColour.UseVisualStyleBackColor = true;
+            this.btnChangeColour.Click += new System.EventHandler(this.btnChangeColour_Click);
+            // 
+            // frmControlObjects
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(352, 255);
+            this.Controls.Add(this.btnChangeColour);
+            this.Controls.Add(this.lblText);
+            this.Name = "frmControlObjects";
+            this.Text = "Control Objects By Tiva Rait";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblText;
+        private System.Windows.Forms.Button btnChangeColour;
+    }
+}
+
---
+++ ControlObjectsTivaR/ControlObjectsForm.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/*
+ * Created by: Tiva Rait
+ * Created on: 13-04-2018
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #24 - Control Objects
+ * This program changes the colour of a button and label when a button is clicked
+*/
+
+namespace ControlObjectsTivaR
+{
+    public partial class frmControlObjects : Form
+    {
+        public frmControlObjects()
+        {
+            InitializeComponent();
+        }
+
+        private void btnChangeColour_Click(object sender, EventArgs e)
+        {
+            //Loop to change the colour
+            foreach (Control aControlObject in this.Controls)
+            {
+                aControlObject.BackColor = Color.Green;
+
+                // if the object is a label then it shall become purple
+                if (aControlObject.GetType() == typeof(Label))
+                {
+                    aControlObject.BackColor = Color.Purple;
+                }
+            }
+
+           /* // if the object is a label then it shall become purple
+            if (aControlObject.GetType()==typeof(Label))
+            {
+                aControlObject.BackColor = Color.Purple;
+            }
+            */
+        }
+    }
+}
